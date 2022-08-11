f705e7b287bb492f856eca6258837f000a2d82b8
papilayi <102283471+papilayi@users.noreply.github.com>
2022-08-11 16:43:56 +0100

    First commit

---
+++ to-do-list/src/App.js
@@ -0,0 +1,98 @@
+import {useState} from 'react'
+import AddTaskForm from './components/AddTaskForm.js'
+import UpdateForm from './components/UpdateForm.js'
+import ToDo from './components/ToDo.js'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import './App.css';
+
+function App() {
+
+  const [toDo, setToDo] = useState([]);
+
+  const [newTask, setNewTask] = useState('');
+  const [updateData, setUpdateData] = useState('');
+
+  const addTask = () => {
+    if(newTask) {
+      let num = toDo.length + 1;
+      let newEntry = {id: num, title: newTask, status: false}
+      setToDo([...toDo, newEntry])
+      setNewTask('');    
+    }
+  }
+
+
+  const deleteTask = (id) => {
+    let newTasks = toDo.filter( task => task.id !== id)
+    setToDo(newTasks)
+  }
+
+  const markDone = (id) => {
+    let newTask = toDo.map( task => {
+      if(task.id === id) {
+        return({ ...task, status: !task.status })
+      }
+      return task;
+    })
+    setToDo(newTask);
+  }
+
+  const cancelUpdate = () => {
+    setUpdateData('');
+  }
+
+  const changeTask = (e) => {
+    let newEntry = {
+      id: updateData.id,
+      title: e.target.value,
+      status: updateData.status ? true : false
+    }
+    setUpdateData(newEntry);
+  }
+
+  const updateTask = ()=> {
+    let filterRecords = [...toDo].filter( task => task.id !== updateData.id);
+    let updateObject = [...filterRecords, updateData]
+    setToDo(updateObject);
+    setUpdateData('');
+  }
+
+
+  return (
+    <div className="container App">
+      <br></br>
+      <h2>To Do List App</h2>
+      <br></br>
+
+        {/* Update Task */}
+    {updateData && updateData ? (
+      <UpdateForm
+        updateData={updateData} 
+        changeTask={changeTask} 
+        updateTask={updateTask}
+        cancelUpdate={cancelUpdate}>        
+      </UpdateForm>
+        ) : (
+      <AddTaskForm
+        newTask={newTask} 
+        setNewTask ={setNewTask}
+        addTask={addTask}>
+      </AddTaskForm>
+    )}
+    
+    {toDo && toDo.length ? '' : 'No Tasks.....'}
+
+    <ToDo
+      toDo={toDo} 
+      markDone={markDone}
+      setUpdateData={setUpdateData} 
+      deleteTask={deleteTask}>
+    </ToDo>
+    </div>
+  );
+}
+
+export default App;
---
+++ to-do-list/src/components/AddTaskForm.js
@@ -0,0 +1,26 @@
+const AddTaskForm = ({ newTask, setNewTask, addTask }) => {
+  return(
+    <>
+    {/* Add Task */}
+    <div className='row'>
+        <div className='col'>
+            <input 
+              value={newTask}
+              onChange={ (e) => setNewTask(e.target.value)}  
+              className='form-control form-control-lg'>  
+            </input>
+          </div>
+          <div className='col-auto'>
+            <button 
+               onClick={addTask}
+                className='btn btn-lg btn-success'>
+                Add Task
+            </button>
+          </div>
+    </div>
+        <br></br>
+    </>
+  )
+}
+
+export default AddTaskForm;
